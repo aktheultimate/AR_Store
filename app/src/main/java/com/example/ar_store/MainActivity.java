5085b23b964970635cef9243a96056211c05555a
Akash <akash.me23@gmail,com>
2022-12-28 06:03:33 +0530

    V1.1

--- app/src/main/java/com/example/ar_store/MainActivity.java
+++ app/src/main/java/com/example/ar_store/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    public Button btn1,btn2,btn3,btn4,btn5;
+    public Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btn10;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +17,12 @@ public class MainActivity extends AppCompatActivity {
         btn2 = findViewById(R.id.button2);
         btn3 = findViewById(R.id.button3);
         btn4 = findViewById(R.id.button4);
+        btn5 = findViewById(R.id.button5);
+        btn6 = findViewById(R.id.button6);
+        btn7 = findViewById(R.id.button7);
+        btn8 = findViewById(R.id.button8);
+        btn9 = findViewById(R.id.button9);
+        btn10 = findViewById(R.id.button10);
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,6 +56,60 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, displayAR.class);
                 intent.putExtra("product", "personal_computer");
+                startActivity(intent);
+            }
+        });
+
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, displayAR.class);
+                intent.putExtra("product", "Laptop");
+                startActivity(intent);
+            }
+        });
+
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, displayAR.class);
+                intent.putExtra("product", "Sofa");
+                startActivity(intent);
+            }
+        });
+
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, hatAR.class);
+                intent.putExtra("product", "Hat");
+                startActivity(intent);
+            }
+        });
+
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, displayAR.class);
+                intent.putExtra("product", "Table");
+                startActivity(intent);
+            }
+        });
+
+        btn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, HeadphoneAR.class);
+                intent.putExtra("product", "Headphones");
+                startActivity(intent);
+            }
+        });
+
+        btn10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, displayAR.class);
+                intent.putExtra("product", "PS5");
                 startActivity(intent);
             }
         });
